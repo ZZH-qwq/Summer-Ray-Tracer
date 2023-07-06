87f53d62999963e5895d051b6f4452328e5c302e
ZZH-qwq <zhouzihan_mailbox@163.com>
2023-07-06 10:05:05 +0800

    camera and multisampling antialising

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -3,16 +3,19 @@ use image::{ImageBuffer, RgbImage};
 use indicatif::ProgressBar;
 use std::{fs::File, process::exit};
 
-mod vec3;
-use vec3::{Color, Vec3};
-mod ray;
-use ray::Ray;
+mod camera;
 mod hittable;
-use crate::hittable::*;
-mod sphere;
-use sphere::Sphere;
 mod hittable_list;
+mod ray;
+mod sphere;
+mod vec3;
+use camera::Camera;
+use hittable::*;
 use hittable_list::HittableList;
+use rand::Rng;
+use ray::Ray;
+use sphere::Sphere;
+use vec3::{Color, Vec3};
 
 // 接受一个光线做为参数 然后计算这条光线所产生的颜色
 fn ray_color(ray: Ray, world: &dyn Hittable) -> Color {
@@ -30,9 +33,10 @@ fn main() {
     let aspect_ratio = 16.0 / 9.0;
     let width = 400;
     let height = (width as f64 / aspect_ratio) as u32;
+    let samples_per_pixel = 100;
 
     // 生成
-    let path = std::path::Path::new("output/book1/image5.jpg");
+    let path = std::path::Path::new("output/book1/image6.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
     let quality = 100;
@@ -49,33 +53,24 @@ fn main() {
     world.add(Box::new(Sphere::new(Vec3::new(0.0, -100.5, -1.0), 100.0)));
 
     // 镜头
-    let viewport_height = 2.0;
-    let viewport_width = viewport_height * aspect_ratio;
-    let focal_length = 1.0;
-
-    let origin = Vec3::zero();
-    let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, viewport_height, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, focal_length);
+    let cam = Camera::new();
 
     for j in 0..height {
-        // y方向进度
-        let v: f64 = (j as f64) / ((height - 1) as f64);
-
         for i in 0..width {
-            // x方向进度
-            let u: f64 = (i as f64) / ((width - 1) as f64);
             let pixel = img.get_pixel_mut(i, height - 1 - j);
+            let mut pixel_color = Vec3::zero();
+            for _ in 0..samples_per_pixel {
+                // x,y方向分量 加入了多重采样抗锯齿
+                let u_rand: f64 = rand::thread_rng().gen();
+                let v_rand: f64 = rand::thread_rng().gen();
+                let u: f64 = (i as f64 + u_rand) / ((width - 1) as f64);
+                let v: f64 = (j as f64 + v_rand) / ((height - 1) as f64);
 
-            // 生成光线
-            let ray = Ray::new(
-                origin,
-                lower_left_corner + horizontal * u + vertical * v - origin,
-            );
-
-            let color = ray_color(ray, &world);
-            let rgb = color.to_u8();
+                // 生成光线
+                let ray = cam.get_ray(u, v);
+                pixel_color += ray_color(ray, &world);
+            }
+            let rgb = (pixel_color / samples_per_pixel as f64).to_u8();
             *pixel = image::Rgb([rgb.0, rgb.1, rgb.2]);
         }
         progress.inc(1);
