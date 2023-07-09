8700eb4319dc54b35d6e86047098801e3b049060
ZZH-qwq <zhouzihan_mailbox@163.com>
2023-07-10 00:01:19 +0800

    b2image8 - trilinear interpolation

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -166,7 +166,7 @@ fn main() {
     let max_depth = 50;
 
     // 生成
-    let path = std::path::Path::new("output/book2/image7.jpg");
+    let path = std::path::Path::new("output/book2/image8.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
     let quality = 100;
--- raytracer/src/perlin.rs
+++ raytracer/src/perlin.rs
@@ -33,11 +33,44 @@ impl Perlin {
         p
     }
 
-    pub fn noise(&self, p: Vec3) -> f64 {
-        let i = ((4.0 * p.x) as i32 & 255) as usize;
-        let j = ((4.0 * p.y) as i32 & 255) as usize;
-        let k = ((4.0 * p.z) as i32 & 255) as usize;
+    fn trilinear_interp(c: Vec<Vec<Vec<f64>>>, u: f64, v: f64, w: f64) -> f64 {
+        let mut accum = 0.0;
+        for i in 0..2 {
+            for j in 0..2 {
+                for k in 0..2 {
+                    accum += (i as f64 * u + (1.0 - i as f64) * (1.0 - u))
+                        * (j as f64 * v + (1.0 - j as f64) * (1.0 - v))
+                        * (k as f64 * w + (1.0 - k as f64) * (1.0 - w))
+                        * c[i][j][k];
+                }
+            }
+        }
+        accum
+    }
 
-        self.ranfloat[self.perm_x[i] ^ self.perm_y[j] ^ self.perm_z[k]]
+    pub fn noise(&self, p: Vec3) -> f64 {
+        let u = p.x - p.x.floor();
+        let v = p.y - p.y.floor();
+        let w = p.z - p.z.floor();
+
+        let i = p.x.floor() as i32;
+        let j = p.y.floor() as i32;
+        let k = p.z.floor() as i32;
+
+        let mut c = vec![vec![vec![0.0; 2]; 2]; 2];
+
+        for di in 0..2 {
+            for dj in 0..2 {
+                for dk in 0..2 {
+                    let x = ((i + di) & 255) as usize;
+                    let y = ((j + dj) & 255) as usize;
+                    let z = ((k + dk) & 255) as usize;
+                    c[di as usize][dj as usize][dk as usize] =
+                        self.ranfloat[self.perm_x[x] ^ self.perm_y[y] ^ self.perm_z[z]];
+                }
+            }
+        }
+
+        Self::trilinear_interp(c, u, v, w)
     }
 }
